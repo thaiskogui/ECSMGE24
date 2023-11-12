9a61b69652945b679ecc62dc018a32e226fad197
thaiskogui <147950906+thaiskogui@users.noreply.github.com>
2023-11-12 17:03:04 -0300

    ECSMGE24.py

--- ECSMGE24.py
+++ ECSMGE24.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# CÓDIGO COM FUNÇÃO PARA RODAR UMA PASTA COM VÁRIAS IMAGENS
+# CODE WITH FUNCTION TO PROCESS A FOLDER WITH MULTIPLE IMAGES
 
 # In[48]:
 
@@ -16,10 +16,10 @@ from skimage.segmentation import watershed
 from scipy import ndimage as ndi
 import pandas as pd
 
-# Diretório das imagens
+# Directory of images
 diretorio_das_imagens = '...\\images\\'
 
-# Função para calcular a área do menor círculo circunscrito a uma partícula
+# Function to calculate the area of the smallest circle circumscribed around a particle
 def calcular_area_circunscrita(mascara):
     cnts = cv2.findContours(mascara.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
 
@@ -28,15 +28,15 @@ def calcular_area_circunscrita(mascara):
 
     c = max(cnts, key=cv2.contourArea)
 
-    # Encontrar o menor círculo circunscrito
+    # Find the smallest circumscribed circle
     ((_, _), raio) = cv2.minEnclosingCircle(c)
 
-    # Calcular a área do círculo
+    # Calculate the area of the circle
     area_circunscrita = np.pi * (raio ** 2)
 
     return area_circunscrita
 
-# Função para calcular áreas das partículas
+# Function to calculate areas of particles
 def calcular_area_particulas(labels, otsu):
     resultados = []
     for label in np.unique(labels):
@@ -51,48 +51,48 @@ def calcular_area_particulas(labels, otsu):
         resultados.append((label, area, area_circunscrita))
     return resultados
 
-# Função principal
+# Main function
 def funcao_esfericidade(diretorio_das_imagens):
     dados_resultados = []
 
-    # Lista todos os arquivos no diretório
+    # List all files in the directory
     arquivos_imagens = os.listdir(diretorio_das_imagens)
 
-    # Itera sobre os arquivos e processa cada imagem
+    # Iterate over the files and process each image
     for arquivo in arquivos_imagens:
         # Verifica se o arquivo é uma imagem (pode adicionar mais extensões se necessário)
         if arquivo.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
             # Constrói o caminho completo para a imagem
             caminho_completo = os.path.join(diretorio_das_imagens, arquivo)
 
-            # Abrindo a imagem
+            # Open the image
             imagem = Image.open(caminho_completo)
 
-            # Conversão da imagem em "array numpy" para visualização e processamento
+            # Convert the image to a numpy array for visualization and processing
             imagem_array = np.array(imagem)
 
-            # Inversão das cores
+            # Invert the colors
             inv_imagem_array = 255 - imagem_array
 
-            # Desfoque gaussiano
+            # Gaussian blur
             desfoque = cv2.GaussianBlur(inv_imagem_array, (5, 5), 0)
 
-            # Método de Otsu - limiar global, ou seja, em toda a imagem, de forma automática
+            # Otsu's method - global threshold, i.e., over the entire image, automatically
             valor, otsu = cv2.threshold(desfoque, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
-            # Plotando a matriz de distância
+            # Plotting the distance matrix
             dist = ndi.distance_transform_edt(otsu)
 
-            # Localizando os máximos locais na matriz de distância
+            # Locating local maxima in the distance matrix
             local_max = peak_local_max(dist, indices=False, min_distance=20, labels=otsu)
 
-            # Rotulando os máximos locais
+            # Labeling local maxima
             markers = ndi.label(local_max, structure=np.ones((3, 3)))[0]
 
-            # Aplicando watershed
+            # Applying watershed
             labels = watershed(-dist, markers, mask=otsu)
 
-            # Individualizando as partículas
+            # Individualizing particles
             img_final = labels.copy()
             for label in np.unique(labels):
                 if label == 0:
@@ -105,24 +105,24 @@ def funcao_esfericidade(diretorio_das_imagens):
                 cv2.putText(img_final, "{}".format(label), (int(x) - 1, int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1,
                             (255, 255, 200), 2)
 
-            # Calcular áreas das partículas
+            # Calculate particle areas
             resultados_areas = calcular_area_particulas(labels, otsu)
 
-            # Adicionar resultados à lista geral
+            # Add results to the overall list
             for label, area, area_circunscrita in resultados_areas:
                 dados_resultados.append((arquivo, label, area, area_circunscrita))
 
-    # Converter a lista de dados em um DataFrame do pandas
+    # Convert the list of data to a pandas DataFrame
     df_resultados = pd.DataFrame(dados_resultados, columns=['Nome do Arquivo', 'Número da Partícula', 'Área', 'Área Circunscrita'])
 
-    # Especifica o caminho de salvamento do arquivo Excel
+    # Specify the Excel file saving path
     caminho_excel = '...\\Table.xlsx'
 
-    # Exporta o DataFrame para um arquivo Excel
+    # Export the DataFrame to an Excel file
     df_resultados.to_excel(caminho_excel, index=False)
     print(f"O resultado foi salvo em: {caminho_excel}")
 
-# Exemplo de uso da função
+# Example of using the function
 funcao_esfericidade(diretorio_das_imagens)
 
 
